ea4b93bc5a7668b9dd67a325aa0a97021e97f39e
Abstract-a <m.kamari100@gmail.com>
2024-10-19 20:10:11 +0100

    nvm

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
 
   return (
     <>
